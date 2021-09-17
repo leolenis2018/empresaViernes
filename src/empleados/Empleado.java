cdf22acd04f15457c4d9e20a89e4838904b7bd07
leolenis2018 <leolenis2018@gmail.com>
2021-09-17 10:06:16 -0500

    Se codifican opciones 1-3 del Menu

--- src/empleados/Empleado.java
+++ src/empleados/Empleado.java
@@ -12,12 +12,12 @@ package empleados;
 public class Empleado {
     //atributos= datos = variables
     
-    int id;
-    String nombre;
-    String cargo;
-    int salario;
-    String telefono;
-    String direccion;
+    private int id;
+    private String nombre;
+    private String cargo;
+    private int salario;
+    private String telefono;
+    private String direccion;
         
     
     //metodo especial= fotocopia = inicia la clase (Construct0r)
@@ -33,14 +33,60 @@ public class Empleado {
         this.direccion = direccion;
     }
     
-    
-     
+    // METODOS DE ENCAPSULAMIENTO
+    // GET AND SET
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public int getSalario() {
+        return salario;
+    }
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+                  
     //metodos= acciones = funciones
     public int calcularSalario(){
         return(0);
-    }
-    
-
-    
+    }    
     
 }
--- src/empresa/Empresa.java
+++ src/empresa/Empresa.java
@@ -5,6 +5,7 @@
  */
 package empresa;
 
+import empleados.Empleado;
 import java.util.Scanner;
 
 /**
@@ -17,11 +18,14 @@ public class Empresa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         Scanner entrada = new Scanner(System.in);
         int opcion;
-        
-        do{
+        int contador = 0;
+
+        Empleado[] empleados = new Empleado[5];
+
+        do {
             System.out.println("Bienvenido");
             System.out.println("***********");
             System.out.println("1. Agregar empleado");
@@ -29,12 +33,59 @@ public class Empresa {
             System.out.println("3. Mostrar empleados");
             System.out.println("4. Salir");
             System.out.println("");
-            
+
             System.out.print("Digite una opcion:");
             opcion = entrada.nextInt();
-        
-        } while (opcion!=4);
-        
+
+            switch (opcion) {
+                case 1:
+                    //buscar el next line
+                    //con arraylis no tengo que poner condicional
+                    //contador= 0
+                    if (contador <= 10) 
+                        empleados[contador] = new Empleado();
+                        System.out.println("Digite el id del empleado:");
+                        empleados[contador].setId(entrada.nextInt());
+
+                        System.out.println("Digita Nombre:");
+                        empleados[contador].setNombre(entrada.next());
+
+                        System.out.println("Digita cargo:");
+                        empleados[contador].setCargo(entrada.next());
+
+                        System.out.println("Digita Salario:");
+                        empleados[contador].setSalario(entrada.nextInt());
+
+                        System.out.println("Digita Telefono:");
+                        empleados[contador].setTelefono(entrada.next());
+
+                        System.out.println("Digita Direccion:");
+                        empleados[contador].setDireccion(entrada.next());
+                        contador++;
+                    
+                    
+                        break;
+                    
+            
+            case 2:
+                    break;
+                case 3:
+                    for(int i=0; i<contador; i++)
+                        System.out.println(empleados[i].getNombre());
+                    // para mostrar todo me sirve un ciclo for
+                    break;
+                case 4:
+                    // Poner Mensaje
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Opcion no validad");
+                    break;
+            }
+
+        } while (opcion != 4);
+
+        }
+
+
     }
-    
-}
